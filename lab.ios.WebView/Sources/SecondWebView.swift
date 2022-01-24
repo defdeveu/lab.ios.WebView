d75fabc68a7013e218bffb3641d2d1222941ccf3
Yuliia Hanol <yhanol@anwb.nl>
2022-01-24 10:17:36 +0100

    added url opening in a different browser

--- lab.ios.WebView/Sources/SecondWebView.swift
+++ lab.ios.WebView/Sources/SecondWebView.swift
@@ -2,17 +2,21 @@ import SwiftUI
 import WebKit
 
 struct SecondWebView: View {
+    private let viewModel: SecondWebViewModel = SecondWebViewModel()
+
     var body: some View {
-        WebView_WK(url: URL(string: "https://www.google.com")!)
+        WebView_WK(navigationDelegate: viewModel, url: URL(string: SecondWebViewModel.defaultURLString)!)
     }
 }
 
 struct WebView_WK: UIViewRepresentable {
 
+    weak var navigationDelegate: WKNavigationDelegate?
     var url: URL
 
     func makeUIView(context: Context) -> WKWebView {
         let webView = WKWebView()
+        webView.navigationDelegate = navigationDelegate
         webView.configuration.preferences.isFraudulentWebsiteWarningEnabled = false
         webView.configuration.preferences.javaScriptCanOpenWindowsAutomatically = true
         webView.configuration.preferences.setValue(true, forKey: "allowFileAccessFromFileURLs")
