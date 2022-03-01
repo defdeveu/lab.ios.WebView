e52a9ee53e8c1417cf9a2dfa6fb60883a825dac9
zs <zs@defdev>
2022-03-01 21:54:41 +0100

    WKWebView new version

--- lab.ios.WebView/Sources/SecondWebView.swift
+++ lab.ios.WebView/Sources/SecondWebView.swift
@@ -2,25 +2,17 @@ import SwiftUI
 import WebKit
 
 struct SecondWebView: View {
-    private let viewModel: SecondWebViewModel = SecondWebViewModel()
-
     var body: some View {
-        WebView_WK(navigationDelegate: viewModel, url: URL(string: SecondWebViewModel.defaultURLString)!)
+        WebView_WK(url: URL(string: "https://zs.labs.defdev.eu/eula.html")!)
     }
 }
 
 struct WebView_WK: UIViewRepresentable {
 
-    weak var navigationDelegate: WKNavigationDelegate?
     var url: URL
 
     func makeUIView(context: Context) -> WKWebView {
-        let webView = WKWebView()
-        webView.navigationDelegate = navigationDelegate
-        webView.configuration.preferences.isFraudulentWebsiteWarningEnabled = false
-        webView.configuration.preferences.javaScriptCanOpenWindowsAutomatically = true
-        webView.configuration.preferences.setValue(true, forKey: "allowFileAccessFromFileURLs")
-        return webView
+        return WKWebView()
     }
 
     func updateUIView(_ webView: WKWebView, context: Context) {
